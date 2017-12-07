91bec084e8207673b872eda38d3322a7c052ae63
mkieboom <mkieboom@mapr.com>
2017-12-07 20:15:40 +0100

    Improved capture and streaming

--- webcamcapture.sh
+++ webcamcapture.sh
@@ -13,13 +13,13 @@ while true; do
   curl -i -X PUT "http://$MAPR_HOST:14000/webhdfs/v1/webcam/$filename?op=CREATE&user.name=mapr"
   curl -i -X PUT -T $outputfolder/$filename -H "Content-Type:application/octet-stream" "http://$MAPR_HOST:14000/webhdfs/v1/webcam/$filename?op=CREATE&overwrite=true&permission=550&data=true&user.name=mapr"
 
-  echo "Pushing new file ('$filename') event on MapR Streams using Kafka REST API"
+  echo "Pushing new file ('"$filename"') event on MapR Streams using Kafka REST API"
   curl -X POST -H "Content-Type: application/vnd.kafka.json.v1+json" \
      --data '{"records":[{"value": {"filename" : "'$filename'" , "path" : "'$outputfolder'"}  }]}' \
      http://$MAPR_USER:$MAPR_PASSWORD@$MAPR_HOST:8082/topics/%2F$MAPR_STREAM%3A$MAPR_STREAM_TOPIC
 
   # Once processed, remove the file to avoid the filesystem to fload
-  echo "Removing file '$filename'"
+  echo "Removing file '"$filename"'"
   rm -rf $outputfolder/$filename
 
 done;
