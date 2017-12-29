5517ac1393c4093d138f86f825066e49797264ed
mkieboom <mkieboom@mapr.com>
2017-12-29 19:48:11 +0100

    Fixed webcam capture and streaming

--- webcamcapture.sh
+++ webcamcapture.sh
@@ -16,14 +16,14 @@ while true; do
     if [ -f "$outputfolder/$filename" ]
     then
       # Push the captured image to MapR-FS
-      curl -i -X PUT "http://$MAPR_HOST:14000/webhdfs/v1/webcam/$filename?op=CREATE&permission=444&user.name=mapr"
-      curl -i -X PUT -T $outputfolder/$filename -H "Content-Type:application/octet-stream" "http://$MAPR_HOST:14000/webhdfs/v1/webcam/$filename?op=CREATE&overwrite=true&op=SETPERMISSION&permission=444&data=true&user.name=mapr"
+      curl -i -X PUT "http://$MAPR_HOST:14000/webhdfs/v1$MAPR_VOLUME$filename?op=CREATE&permission=444&user.name=mapr"
+      curl -i -X PUT -T $outputfolder/$filename -H "Content-Type:application/octet-stream" "http://$MAPR_HOST:14000/webhdfs/v1$MAPR_VOLUME$filename?op=CREATE&overwrite=true&op=SETPERMISSION&permission=444&data=true&user.name=mapr"
 
       # Push an event on MapR Streams to tell a new image has been uploaded
       echo "Pushing new file ('"$filename"') event on MapR Streams using Kafka REST API"
       curl -X POST -H "Content-Type: application/vnd.kafka.json.v1+json" \
-         --data '{"records":[{"value": {"filename" : "'$filename'" , "path" : "'$outputfolder'"}  }]}' \
-         http://$MAPR_USER:$MAPR_PASSWORD@$MAPR_HOST:8082/topics/%2F$MAPR_STREAM%3A$MAPR_STREAM_TOPIC
+         --data '{"records":[{"value": {"fileInfo": {"filename" : "'$filename'" , "path" : "'$outputfolder'"}}}]}' \
+         http://$MAPR_USER:$MAPR_PASSWORD@$MAPR_HOST:8082/topics/$MAPR_STREAM%3A$MAPR_STREAM_TOPIC
       echo "\nPush finished"
 
       # Once processed, remove the file to avoid the filesystem to fload
@@ -31,10 +31,10 @@ while true; do
       rm -rf $outputfolder/$filename
     else
       echo "No webcam image found. Is the webcam connected? Pausing for 5 seconds"
-	  sleep 5
+    sleep 5
     fi
   #else
-  #	echo "No webcam connected to /dev/video0. Pausing for 5 seconds"
+  # echo "No webcam connected to /dev/video0. Pausing for 5 seconds"
   #  sleep 5
   #fi
 done;
