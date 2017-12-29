b417f0ae40dee10166611c16046c6180c30c16f1
mkieboom <mkieboom@mapr.com>
2017-12-29 21:05:01 +0100

    Added extra logging

--- webcamcapture.sh
+++ webcamcapture.sh
@@ -21,11 +21,20 @@ while true; do
       curl -i -X PUT "http://$MAPR_HOST:14000/webhdfs/v1$MAPR_VOLUME$filename?op=CREATE&permission=444&user.name=mapr"
       curl -i -X PUT -T $outputfolder/$filename -H "Content-Type:application/octet-stream" "http://$MAPR_HOST:14000/webhdfs/v1$MAPR_VOLUME$filename?op=CREATE&overwrite=true&op=SETPERMISSION&permission=444&data=true&user.name=mapr"
 
+      # Logging to /tmp/webcam/webcam_docker.log
+      echo curl -i -X PUT "http://$MAPR_HOST:14000/webhdfs/v1$MAPR_VOLUME$filename?op=CREATE&permission=444&user.name=mapr" > /tmp/webcam/webcam_docker.log
+      echo curl -i -X PUT -T $outputfolder/$filename -H "Content-Type:application/octet-stream" "http://$MAPR_HOST:14000/webhdfs/v1$MAPR_VOLUME$filename?op=CREATE&overwrite=true&op=SETPERMISSION&permission=444&data=true&user.name=mapr" >> /tmp/webcam/webcam_docker.log
+
       # Push an event on MapR Streams to tell a new image has been uploaded
       echo "Pushing new file ('"$filename"') event on MapR Streams using Kafka REST API"
       curl -X POST -H "Content-Type: application/vnd.kafka.json.v1+json" \
          --data '{"records":[{"value": {"fileInfo": {"filename" : "'$filename'" , "path" : "'$outputfolder'"}}}]}' \
          http://$MAPR_USER:$MAPR_PASSWORD@$MAPR_HOST:8082/topics/$MAPR_VOLUME_URLENCODED$MAPR_STREAM%3A$MAPR_STREAM_TOPIC
+
+      # Logging to /tmp/webcam/webcam_docker.log
+      echo curl -X POST -H "Content-Type: application/vnd.kafka.json.v1+json" \
+         --data '{"records":[{"value": {"fileInfo": {"filename" : "'$filename'" , "path" : "'$outputfolder'"}}}]}' \
+         http://$MAPR_USER:$MAPR_PASSWORD@$MAPR_HOST:8082/topics/$MAPR_VOLUME_URLENCODED$MAPR_STREAM%3A$MAPR_STREAM_TOPIC >> /tmp/webcam/webcam_docker.log
       echo "\nPush finished"
 
       # Once processed, remove the file to avoid the filesystem to fload
